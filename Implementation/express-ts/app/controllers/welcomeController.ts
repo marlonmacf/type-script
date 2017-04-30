bb26298ff29697150d5e3a60fd275db214b1f8aa
Marlon Andrel <marlonmacf@gmail.com>
2017-04-30 18:42:55 -0300

    Created an express typescript app.

---
+++ Implementation/express-ts/app/controllers/welcomeController.ts
@@ -0,0 +1,21 @@
+// Import only what we need from express.
+import {Router, Request, Response} from 'express';
+
+// Assign router to the express.Router() instance.
+const router: Router = Router();
+
+router.get('/', (request: Request, response: Response) => {
+    response.send('Hello World!');
+});
+
+router.get('/:name', (request: Request, response: Response) => {
+
+    // Extract the name from the request parameters.
+    let {name} = request.params;
+
+    // Greet the given name.
+    response.send(`Hello, ${name}!`);
+});
+
+// Export the express.Router() instance to be used by server.ts.
+export const WelcomeController: Router = router;
